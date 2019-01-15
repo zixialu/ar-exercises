5a7e21d36d9888cf69698fd976b97cb430e21c08
Zixia Lu <zixialu@gmail.com>
2019-01-15 17:08:05 +0000

    Complete exercise 4

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,19 +4,19 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
-burnaby = Store.create(
+Store.create(
   name: "Burnaby",
   annual_revenue: 300_000,
   mens_apparel: true,
   womens_apparel: true
 )
-richmond = Store.create(
+Store.create(
   name: "Richmond",
   annual_revenue: 1_260_000,
   mens_apparel: false,
   womens_apparel: true
 )
-gastown = Store.create(
+Store.create(
   name: "Gastown",
   annual_revenue: 190_000,
   mens_apparel: true,
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,33 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(
+  name: "Surrey",
+  annual_revenue: 224_000,
+  mens_apparel: false,
+  womens_apparel: true
+)
+Store.create(
+  name: "Whistler",
+  annual_revenue: 1_900_000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+Store.create(
+  name: "Yaletown",
+  annual_revenue: 430_000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+@mens_stores = Store.where(mens_apparel: true)
+puts "Mens apparel stores:"
+@mens_stores.each { |store| puts "#{store.name}: #{store.annual_revenue}" }
+
+@womens_stores = Store.where(
+  "womens_apparel = ? AND annual_revenue < ?",
+  true,
+  1_000_000
+)
+puts "Womens apparel stores under $1M revenue:"
+@womens_stores.each { |store| puts "#{store.name}: #{store.annual_revenue}" }
